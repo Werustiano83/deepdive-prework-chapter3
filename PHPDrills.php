991a4c16b954448af1702ac7c3abc30bea44d6b1
Raul Villarreal <rvillarrcal@cnm.edu>
2016-07-04 10:54:13 -0600

    Creation of .gitignore file Created drills chapter 3 I think I fixed it It's working now updated new drills

--- PHPDrills.php
+++ PHPDrills.php
@@ -6,9 +6,42 @@
 		<body>
 			<p>I'm BACK</p>
 		</body>
+	<p>
+		<?php
+		// Rounding a floating point number to an integer
+		// and print it to the screen
+		$redondo = round(5.35698);
+		print $redondo;
+		?>
+	</p>
+	<p>
+		<?php
+		// Rounding a floating point number to 3 decimal places
+		// and print it to the screen
+		$redondo3 = round(9.456395, 3);
+		print $redondo3;
+		?>
+	</p>
+	<p>
+		<?php
+		// Print out the position of a letter that is in
+		// your own name
+		//should return 2
+		$posicion = strpos("armando", "m");
+		echo $posicion;
 
-<?php
-	echo "I hope this works nowkhfkjhdflsdgjnsd"
-?>
-	</html>
+		?>
+	</p>
+	<p>
+		<?php
+		// Check for a false value of a letter that is not
+		// in your own name and print out an error message
+		//should return false and print "try another string"
+		if (strpos("armando", "j") === false) {
+			print "Try another letter not 'j'.";
+		}
+		?>
+	</p>
+</html>
+
 
